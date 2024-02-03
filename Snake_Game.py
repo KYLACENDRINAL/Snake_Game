eb318d9781fabb4f24c2105e978c273d120f8baa
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-02-03 10:19:56 +0800

    pseudocode

---
+++ Snake_Game.py
@@ -0,0 +1,14 @@
+# A snake game that will display the score, high score and levels of the game.
+
+# pseudocode
+# Set up the screen
+# Create the snake head
+# Define the function to move the snake head
+# Set up the keyboard bindings
+# Create the snake food
+# Check collision with the food
+# Create the segments
+# Check the collision with the border
+# Check the collision with body segments
+# Set up the score
+# Create the level
