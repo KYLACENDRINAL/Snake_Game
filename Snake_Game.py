9017d6480edcadc578956c5d9a51bb7e7c1a3f24
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-02-03 10:37:54 +0800

    Created the snake head

--- Snake_Game.py
+++ Snake_Game.py
@@ -10,10 +10,20 @@ wn.bgcolor("pink")
 wn.setup(width=600, height=600)
 wn.tracer
 
+
+# Create the snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("circle")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
 wn.mainloop()
 
 
-# Create the snake head
+
 # Define the function to move the snake head
 # Set up the keyboard bindings
 # Create the snake food
